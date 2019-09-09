f2ca00a7f76c6a1833b24a3e8b6b20c0d58afbc5
Edison Jwa <i@uv.uy>
2019-09-09 18:56:28 +0900

    :tada: New Styles

--- src/App.js
+++ src/App.js
@@ -1,117 +1,157 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
-import { Layout, Typography, Button, Alert } from 'antd';
+import { Layout, Typography, Button, notification, Row, Col, Card } from 'antd';
 import Headers from './Header'
-import QRCodeReact from 'qrcode.react'
+import QRCode from 'qrcode.react'
 import DonateTables from './DonateTables'
 
-const { Title, Text } = Typography;
+const { Title, Text, Paragraph } = Typography;
 const { Footer } = Layout;
 
 function App() {
   return (
-      <Layout style={{ minHeight:"100vh" }}>
-        <Router>
-          <Headers />
-          <Layout>
-            <Layout style={{ padding: "24px" }}>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Router>
+        <Headers />
+        <Layout>
+          <Layout style={{ padding: "24px" }}>
+            <Row gutter={48}>
               <Switch>
                 <Route path="/" exact component={Index} />
-                <Route path="/Alipay" component={Alipay} />
-                <Route path="/AlipayOrder" component={AlipayOrder} />
-                <Route path="/AlipayCoupon" component={AlipayCoupon} />
+                <Route path="/Alipay" component={AlipayPage} />
                 <Route path="/WeChatPay" component={WeChatPay} />
-                <Route path="/UnionPayCoupon" component={UnionPayCoupon} />
-                <Route path="/UnionPay" component={UnionPay} />
+                <Route path="/UnionPay" component={UnionPayPage} />
                 <Route component={NoMatch} />
               </Switch>
-            </Layout>
+            </Row>
           </Layout>
-        </Router>
-        <Footer style={{ textAlign: 'center' }}>Copyright © 2015 - 2019 <a href="https://uv.uy" target="_blank" rel="external noopener noreferrer">Edison Jwa</a>. All rights reserved.</Footer>
-      </Layout>
-    
+        </Layout>
+      </Router>
+      <Footer style={{ textAlign: 'center' }}>Copyright © 2015 - 2019 <a href="https://uv.uy" target="_blank" rel="external noopener noreferrer">Edison Jwa</a>. All rights reserved.</Footer>
+    </Layout>
+
   );
 }
 
 export default App;
 
-function Index() {
-  return <div id="Index" >
-    <div align="center">
-    <Title level={3}>赞助与付款</Title><br />
-    <Text>感谢您的支持。</Text><br /><br />
-    <Text>目前接受的付款方式有 支付宝，<Text delete>微信</Text>，银联云闪付</Text><br /><br />
-    <Text>赞助列表将展示在此处</Text><br /><br />
-    <Text>金额小于1元人民币的交易信息将不会记录。</Text><br /><br />
-    <Text>很难准时更新，但是我会尽可能避免遗漏。</Text><br /><br />
-    <Title level={4}>赞助列表</Title><br />
+class Index extends React.Component {
+  render() {
+    return <div id="Index" >
+      <Col xs={48} sm={48} md={12} lg={12} xl={12} xxl={12} >
+
+        <Card title="赞助与付款" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }}>
+          <Paragraph>
+            <Text>感谢您的支持。</Text>
+            <ul>
+              <li>目前接受的付款方式有 支付宝，<Text delete>微信</Text>，银联云闪付</li>
+              <li>赞助列表将展示在此处</li>
+              <li>金额小于1元人民币的交易信息将不会记录。</li>
+              <li>很难准时更新，但是我会尽可能避免遗漏。</li>
+            </ul>
+          </Paragraph>
+        </Card>
+        <br />
+      </Col>
+      <Col xs={48} sm={48} md={12} lg={12} xl={12} xxl={12}>
+        <Card title="赞助列表" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }}><br />
+          <DonateTables />
+        </Card>
+      </Col>
+
     </div>
-    <DonateTables />
-    
+  }
+}
+
+function AlipayPage() {
+  return <div id="Alipay">
+    <AlipayCoupon />
+    <Alipay />
+    <AlipayOrder />
   </div>;
 }
 
 function Alipay() {
-  return <div id="Alipay" align="center">
-    <Title level={3}>支付宝付款</Title><br />
-    <QRCodeReact renderAs="svg" size="200" value="https://qr.alipay.com/tsx0030028m0ppqbhreqs8e" /><br /><br />
-    <Button type="primary" href="https://qr.alipay.com/tsx0030028m0ppqbhreqs8e" target="_blank" >点击付款</Button>
-  </div>;
+  return <Col xs={48} sm={48} md={12} lg={8} xl={8} xxl={8} >
+    <Card title="支付宝付款" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }} align="center">
+      <QRCode renderAs="svg" size={200} value="https://qr.alipay.com/tsx0030028m0ppqbhreqs8e" /><br /><br />
+      <Button type="primary" href="https://qr.alipay.com/tsx0030028m0ppqbhreqs8e" target="_blank" >点击付款</Button>
+    </Card><br />
+  </Col>
 }
-
 function AlipayOrder() {
-  return <div id="AlipayOrder" align="center">
-    <Title level={3}>支付宝点单</Title><br />
-    <QRCodeReact renderAs="svg" size='200' value="https://qr.alipay.com/00c01809qtt0c2uxrz4qp05" /><br /><br />
-    <Button type="primary" href="https://qr.alipay.com/00c01809qtt0c2uxrz4qp05" target="_blank" >点击下单</Button>
-  </div>;
+  return <Col xs={48} sm={48} md={12} lg={8} xl={8} xxl={8} >
+    <Card title="支付宝点单" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }} align="center">
+      <QRCode renderAs="svg" size={200} value="https://qr.alipay.com/00c01809qtt0c2uxrz4qp05" /><br /><br />
+      <Button type="primary" href="https://qr.alipay.com/00c01809qtt0c2uxrz4qp05" target="_blank" >点击下单</Button>
+    </Card><br />
+  </Col>
 }
 
 function AlipayCoupon() {
-  return <div id="AlipayCoupon" align="center">
-    <Title level={3}>支付宝红包</Title><br />
-    <QRCodeReact renderAs="svg" size="200" value="QUmlc1x06696td2v0womxjkz2e5eQ0" /><br /><br />
-    <Text>打开支付宝扫描上方二维码或<br />在首页搜“ <Text type="danger" strong code copyable>1380937</Text> ”领红包</Text>
-  </div>;
+  return <Col xs={48} sm={48} md={12} lg={8} xl={8} xxl={8} >
+    <Card title="支付宝红包" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }} align="center">
+      <QRCode renderAs="svg" size={200} value="QUmlc1x06696td2v0womxjkz2e5eQ0" /><br />
+      <Text>打开支付宝扫描上方二维码或<br />在首页搜“ <Text type="danger" strong code copyable>1380937</Text> ”领红包</Text>  </Card><br />
+  </Col>
 }
 
-function WeChatPay() {
-  return <div id="WeChatPay" align="center">
-    <Title type="danger" level={3}>暂不支持微信收款</Title>
+class WeChatPay extends React.Component {
+  render() {
+    const openNotificationWithIcon = type => {
+      notification[type]({
+        message: '暂不支持微信支付',
+        description: '我们将在近期支持微信支付',
+      });
+    };
+    return (
+      <div id="WeChatPay" onAnimationStart={() => openNotificationWithIcon('warning')}>
 
+        <Index />
+      </div>
+    )
+  }
+}
+
+function UnionPayPage() {
+  return <div id="UnionPay">
+    <UnionPayCoupon />
+    <UnionPay />
   </div>;
 }
 
 function UnionPayCoupon() {
-  return <div id="UnionPayCoupon" align="center">
-    <Title level={3}>云闪付红包</Title><br />
-    <QRCodeReact renderAs="svg" size="200" value="https://wallet.95516.com/s/wl/webV3/activity/vMarketing2/html/snsIndex.html?r=725aba45364e6cf48c6b044fb9069197" /><br /><br />
-    <Button type="primary" href="https://wallet.95516.com/s/wl/webV3/activity/vMarketing2/html/snsIndex.html?r=725aba45364e6cf48c6b044fb9069197" target="_blank" >点击领取</Button>
-  </div>;
+  return <Col xs={48} sm={48} md={12} lg={12} xl={12} xxl={12}>
+    <Card title="云闪付红包" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }} align="center">
+      <QRCode renderAs="svg" size={200} value="https://wallet.95516.com/s/wl/webV3/activity/vMarketing2/html/snsIndex.html?r=725aba45364e6cf48c6b044fb9069197" /><br /><br />
+      <Button type="primary" href="https://wallet.95516.com/s/wl/webV3/activity/vMarketing2/html/snsIndex.html?r=725aba45364e6cf48c6b044fb9069197" target="_blank" >点击领取</Button>
+    </Card><br />
+  </Col>;
 }
 
 function UnionPay() {
-  return <div id="UnionPay" align="center">
-    <Title level={3}> 云闪付付款</Title><br />
-    <QRCodeReact renderAs="svg" size="200" value="https://qr.95516.com/00010002/01012123571890753265718637730182" /><br /><br />
-    <Text>打开云闪付App、银行App<br />和热门App的首页扫码功能轻松支付</Text>
-  </div>;
+  return <Col xs={48} sm={48} md={12} lg={12} xl={12} xxl={12}>
+    <Card title="云闪付付款" hoverable headStyle={{ fontSize: 20 }} bodyStyle={{ fontSize: 16 }} align="center">
+      <QRCode renderAs="svg" size={200} value="https://qr.95516.com/00010002/01012123571890753265718637730182" /><br />
+      <Text>打开云闪付App、银行App<br />和热门App的首页扫码功能轻松支付</Text>
+    </Card><br />
+  </Col>;
 }
 
-function NoMatch({ location }) {
+class NoMatch extends React.Component {
+  render() {
+    const openNotificationWithIcon = type => {
+      notification[type]({
+        message: '错误',
+        description: '找不到请求的URL ' + window.location.pathname,
+      });
+    };
+    return (
+      <div id="NoMatch" onAnimationStart={() => openNotificationWithIcon('error')}>
 
-  let errorMessage = "找不到请求的URL " + location.pathname;
-
-  return (
-    <div id="NoMatch" align="center">
-      <Alert
-        message="错误"
-        description={errorMessage}
-        type="error"
-        closable /><br />
         <Index />
-    </div>
-  );
+      </div>
+    )
+  }
 }
--- src/Header.js
+++ src/Header.js
@@ -3,48 +3,34 @@ import { BrowserRouter as Link, NavLink } from "react-router-dom";
 import { Layout, Menu, Icon } from 'antd';
 
 const { Header } = Layout;
-const { SubMenu } = Menu;
 const IconFont = Icon.createFromIconfontCN({
   scriptUrl: 'https://at.alicdn.com/t/font_1391414_9vqulpa9ja5.js',
 });
 
 function Headers() {
   return (
-    <Header className="header" style={{ padding: "0px", height: "64px", lineHeight: "64px" }} align="center">
+    <Header className="header" style={{ padding: "0px", height: "64px", lineHeight: "64px" }}>
       <Menu theme="light" mode="horizontal" style={{ lineHeight: "64px" }} className="icons-list">
-      <Menu.Item key="1">
+        <Menu.Item key="home">
           <NavLink to="/">
             <Icon type="home" /> 主页
-              </NavLink></Menu.Item>
-        <SubMenu title={
-          <span className="submenu-title-wrapper">
-            <Icon type="alipay" />
-            支付宝
-          </span>
-        }>
-          <Menu.Item key="Alipay:1" align="center">
-            <NavLink to="/AlipayCoupon">
-              支付宝红包
-              </NavLink>
-          </Menu.Item>
-          <Menu.Item key="Alipay:2" align="center">
-            <NavLink to="/Alipay">支付宝付款</NavLink></Menu.Item>
-          <Menu.Item key="Alipay:3" align="center">
-            <NavLink to="/AlipayOrder">支付宝点单</NavLink></Menu.Item>
-        </SubMenu>
-        <Menu.Item key="2" disabled>
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="Alipay">
+          <NavLink to="/Alipay">
+            <Icon type="alipay" />支付宝
+          </NavLink>
+        </Menu.Item>
+        <Menu.Item key="wechat" disabled>
           <NavLink to="/WeChatPay">
             <Icon type="wechat" /> 微信
-              </NavLink></Menu.Item>
-        <SubMenu title={
-          <span className="submenu-title-wrapper">
-            <IconFont type="icon-yinlian1193427easyiconnet" />
-            银联
-          </span>
-        }>
-          <Menu.Item key="unionpay:1" align="center"><NavLink to="/UnionPayCoupon"> 云闪付红包</NavLink></Menu.Item>
-          <Menu.Item key="unionpay:2" align="center"><NavLink to="/UnionPay"> 云闪付付款</NavLink></Menu.Item>
-        </SubMenu>
+              </NavLink>
+        </Menu.Item>
+        <Menu.Item key="Union">
+          <NavLink to="/UnionPay" >
+            <IconFont type="icon-yinlian1193427easyiconnet" /> 银联
+            </NavLink>
+        </Menu.Item>
       </Menu>
     </Header>
   );
